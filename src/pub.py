52427b5d1af6cb576f0422b85858269c771be9dc
Sacha Ponniah <sponniah812@gmail.com>
2021-11-17 12:36:11 +0000

    Add 2 methods to Pu b class and tests

--- src/pub.py
+++ src/pub.py
@@ -2,6 +2,15 @@ class Pub:
     def __init__(self, name, till):
         self.name = name
         self.till = till
+        self.drinks = []
 
     def increase_till(self, amount):
         self.till += amount
+
+    def add_drink_to_drinks(self, drink):
+        self.drinks.append(drink)
+
+    def find_drink_by_name(self, drink_name):
+        for drink in self.drinks:
+            if drink.name == drink_name:
+                return drink
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,20 +1,32 @@
 import unittest
 from src.pub import Pub
+from src.drink import Drink
 
 class TestPub(unittest.TestCase):
     
     def setUp(self):
         self.pub = Pub("The Prancing Pony", 100.00)
+        self.drink_beer = Drink("Beer", 2.50)
 
     def test_pub_has_name(self):
         self.assertEqual("The Prancing Pony", self.pub.name)
       
-    def test_icrease_till(self):
-        # Arrange
-        # Act
+    def test_increase_till(self):
         self.pub.increase_till(2.50)
-        # Assert
-        expected = 102.50
-        actual = self.pub.till
         self.assertEqual(102.5, self.pub.till)
+
+    def test_pub_drinks_list_initialised_empty(self):
+        self.assertEqual(0, len(self.pub.drinks))
+
+    def test_add_drink_to_drinks(self):
+        self.pub.add_drink_to_drinks(self.drink_beer)
+        self.assertEqual(1, len(self.pub.drinks))
+
+    def test_find_drink_by_name(self):
+        self.pub.add_drink_to_drinks(self.drink_beer)
+        result = self.pub.find_drink_by_name("Beer")
+        self.assertEqual("Beer", result.name)
+
+
+
     
