554fa47404f8ab933888aa5fb91d14baa8c669a3
Marijke Drenth <drenth.marijke@googlemail.com>
2021-11-17 14:06:12 +0200

    Added drink class and tests

--- tests/drink_test.py
+++ tests/drink_test.py
@@ -2,4 +2,13 @@ import unittest
 from src.drink import Drink
 
 class TestDrink(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.drink_beer = Drink("Beer", 2.50)
+        self.drink_wine = Drink("Wine", 1.50)
+        self.drink_spirit = Drink("Spirit", 2.00)
+
+    def test_drink_has_name(self):
+        self.assertEqual("Beer", self.drink_beer.name)
+    
+    def test_drink_has_price(self):
+        self.assertEqual(2.50,self.drink_beer.price)
